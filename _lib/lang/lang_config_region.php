85eb016ac0e16ed70002af8e39ddb9c7d5896725
rene ferrante <rene.ferranten@gmail.com>
2022-04-01 02:10:09 -0300

    add scriptcase common libraries

---
+++ _lib/lang/lang_config_region.php
@@ -0,0 +1,6 @@
+<?php
+  $this->Nm_lang_conf_region = array();
+  $this->Nm_lang_conf_region['en_us;en_us'] = "English - Inglês (Estados Unidos)";
+  $this->Nm_lang_conf_region['pt_br;pt_br'] = "Portuguese/Português (Brasil) - Português (Brasil)";
+  ksort($this->Nm_lang_conf_region);
+?>
